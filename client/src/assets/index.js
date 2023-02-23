a75c07507f82317a805dcfb8e756965f0cbb3416
Mohith N <mohith@Mohiths-MacBook-Air.local>
2023-02-24 02:43:15 +0530

    changed logo

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import openai from './openai.svg';
 
 export {
   download,
   logo,
   preview,
+  openai
 };
